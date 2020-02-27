5239eec665dda6048149707e678e3327f80fab4b
natgeo-wong <natgeo.wong@outlook.com>
2020-02-27 18:23:09 -0500

    Debugged GeoRegions.jl functions

--- src/GeoRegions.jl
+++ src/GeoRegions.jl
@@ -1,13 +1,18 @@
 module GeoRegions
 
+## Modules Used
+using Dates, DelimitedFiles
+
+## Exporting the following functions:
 export
        ispointinregion, isgridinregion, from0360to180, from180to0360,
        regionpoint, regiongrid, regiongridvec,
        regionextract, regionextractpoint, regionextractgrid, regionextractgrid!,
        gregioncopy, gregioninfoload, gregioninfodisplay, gregioninfoall, gregioninfoadd,
-       gregionbounds, gregionfullname, gregionparent, gregionchild
+       gregionbounds, gregionfullname, gregionparent, gregionchild, gregiongridvec
 
 
+## Including other files in the module
 include("isin.jl")
 include("query.jl")
 include("extract.jl")
--- src/query.jl
+++ src/query.jl
@@ -25,12 +25,12 @@ function gregioninfoload()
 end
 
 function gregioninfodisplay(gregioninfo::AbstractArray)
-    @info "$(Dates.now()) - The following gregions are offered in the ClimateEasy.jl"
+    @info "$(Dates.now()) - The following GeoRegions are offered in the GeoRegions.jl"
     for ii = 1 : size(gregioninfo,1); @info "$(Dates.now()) - $(ii)) $(gregioninfo[ii,7])" end
 end
 
 function gregioninfoall()
-    @info "$(Dates.now()) - The following gregions and their properties are offered in the ClimateEasy.jl"
+    @info "$(Dates.now()) - The following GeoRegions and their properties are offered in the GeoRegions.jl"
 
     greginfo = gregioninfoload(); head = ["ID","Parent","N","W","S","E","Full Name"];
     pretty_table(rinfo,head,alignment=:c);
@@ -60,51 +60,80 @@ end
 function gregionbounds(gregID::AbstractString)
     greginfo = gregioninfoload(); gregions = greginfo[:,1]; ID = (gregions .== gregID);
     N,S,E,W = greginfo[ID,[3,5,6,4]];
-    @debug "$(Dates.now()) - The bounds of the gregion are, in [N,S,E,W] format, [$(N),$(S),$(E),$(W)]."
+    @debug "$(Dates.now()) - The bounds of the GeoRegion are, in [N,S,E,W] format, [$(N),$(S),$(E),$(W)]."
     return [N,S,E,W]
 end
 
 function gregionbounds(gregID::AbstractString,greginfo::AbstractArray)
     gregions = greginfo[:,1]; ID = (gregions .== gregID);
     N,S,E,W = greginfo[ID,[3,5,6,4]];
-    @debug "$(Dates.now()) - The bounds of the gregion are, in [N,S,E,W] format, [$(N),$(S),$(E),$(W)]."
+    @debug "$(Dates.now()) - The bounds of the GeoRegion are, in [N,S,E,W] format, [$(N),$(S),$(E),$(W)]."
     return [N,S,E,W]
 end
 
 ## Find Full GeoRegion Name
 
 function gregionfullname(gregID::AbstractString)
-    reginfo = gregioninfoload(); gregions = greginfo[:,1]; ID = (gregions .== gregID);
-    return reginfo[ID,7][1];
+    greginfo = gregioninfoload(); gregions = greginfo[:,1]; ID = (gregions .== gregID);
+    return greginfo[ID,7][1];
 end
 
 function gregionfullname(gregID::AbstractString,greginfo::AbstractArray)
     gregions = greginfo[:,1]; ID = (gregions .== gregID);
-    return reginfo[ID,7][1];
+    return greginfo[ID,7][1];
 end
 
 ## Find GeoRegion Parent
 
 function gregionparent(gregID::AbstractString)
-    reginfo = gregioninfoload(); gregions = greginfo[:,1]; ID = (gregions .== gregID);
-    return reginfo[regid,2][1];
+    greginfo = gregioninfoload(); gregions = greginfo[:,1]; ID = (gregions .== gregID);
+    return greginfo[ID,2][1];
 end
 
 function gregionparent(gregID::AbstractString,greginfo::AbstractArray)
-    gregions = greginfo[:,1]; ID = (gregions .== gregID);
-    return reginfo[regid,2][1];
+    ggregions = greginfo[:,1]; ID = (gregions .== gregID);
+    return greginfo[ID,2][1];
 end
 
 ## Find GeoRegion Children
 
 function gregionchild(gregID::AbstractString)
-    reginfo = gregioninfoload(); gregions = greginfo[:,2]; ID = (gregions .== gregID);
-    cregions = gregions[ID,:]; head = ["ID","Parent","N","W","S","E","Full Name"];
+    greginfo = gregioninfoload(); gregions = greginfo[:,2]; ID = (gregions .== gregID);
+    cregions = greginfo[ID,:]; head = ["ID","Parent","N","W","S","E","Full Name"];
     pretty_table(cregions,head,alignment=:c);
 end
 
 function gregionchild(gregID::AbstractString,greginfo::AbstractArray)
     gregions = greginfo[:,2]; ID = (gregions .== gregID);
-    cregions = gregions[ID,:]; head = ["ID","Parent","N","W","S","E","Full Name"];
+    cregions = greginfo[ID,:]; head = ["ID","Parent","N","W","S","E","Full Name"];
     pretty_table(cregions,head,alignment=:c);
+end
+
+##
+
+function gregiongridvec(reg::AbstractString,lon::Vector{<:Real},lat::Vector{<:Real})
+
+    @debug "$(Dates.now()) - Determining indices of longitude and latitude boundaries in the parent GeoRegion dataset ..."
+    bounds = gregionbounds(reg); igrid = regiongrid(bounds,lon,lat);
+    iN = igrid[1]; iS = igrid[2]; iE = igrid[3]; iW = igrid[4];
+
+    @debug "$(Dates.now()) - Creating vector of latitude indices to extract ..."
+    if     iN < iS; iNS = iN : iS
+    elseif iS < iN; iNS = iS : iN
+    else;           iNS = iN;
+    end
+
+    @debug "$(Dates.now()) - Creating vector of longitude indices to extract ..."
+    if     iW < iE; iWE = iW : iE
+    elseif iW > iE || (iW == iE && bounds[3] != bounds[4])
+        iWE = 1 : (iE + length(lon) + 1 - iW);
+        lon[1:(iW-1)] = lon[1:(iW-1)] .+ 360; lon = circshift(lon,1-iW);
+    else
+        iWE = iW;
+    end
+
+    reginfo = Dict("boundsID"=>igrid,"IDvec"=>[iWE,iNS],"fullname"=>gregionfullname(reg))
+
+    return lon[iWE],lat[iNS],reginfo
+
 end
