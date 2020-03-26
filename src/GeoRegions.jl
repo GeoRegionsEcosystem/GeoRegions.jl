3928d4fb3076c92cfa1be7dc9cfecc94ce496a93
Nathanael Wong <natgeo.wong@outlook.com>
2020-03-26 01:39:56 -0400

    Due to extensive changes and new features, releasing new minor version.

--- src/GeoRegions.jl
+++ src/GeoRegions.jl
@@ -6,12 +6,11 @@ using DelimitedFiles
 
 ## Exporting the following functions:
 export
-       ispointinregion, isgridinregion, from0360to180, from180to0360,
+       ispointinregion, isgridinregion, isgeoregion, from0360to180, from180to0360,
        regionpoint, regiongrid, regiongridvec,
        regionextract, regionextractpoint, regionextractgrid, regionextractgrid!,
        gregioncopy, gregioninfoload, gregioninfodisplay, gregioninfoall, gregioninfoadd,
        gregionbounds, gregionfullname, gregionparent, gregionchild, gregiongridvec
-
 
 ## Including other files in the module
 include("isin.jl")
