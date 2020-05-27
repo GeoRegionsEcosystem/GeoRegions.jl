16456d7971eedaf463de68afa61ad1a681b9590f
Nathanael Wong <natgeo.wong@outlook.com>
2020-05-27 01:22:47 -0400

    Added docstrings, debugged ispointinregion and isgridinregion.

--- src/isin.jl
+++ src/isin.jl
@@ -9,9 +9,24 @@ Other additional features include:
 
 ## Is Point in GeoRegion?
 
+"""
+    ispointinregion(plon, plat, rlon, rlat; throw=true) -> Bool
+
+Check if a point with coordinates (`plon`,`plat`) is found within a region defined by the longitude and latitude vectors `rlon` and `rlat`
+
+Arguments:
+* `plon::Real` : Longitude of the point in question.
+* `plat::Real` : Latitude of the point in question.
+* `rlon::Vector{<:Real}` : Longitude vector spanning the region.  Points should be evenly spaced.
+* `rlat::Vector{<:Real}` : Latitude vector spanning the region.  Points should be evenly spaced.
+
+Keyword Arguments:
+* `throw::Bool` : If `throw=true`, then if (`plon`,`plat`) is not within the region, an error is thrown and the program stops running.
+"""
 function ispointinregion(
     plon::Real, plat::Real,
-    rlon::Vector{<:Real}, rlat::Vector{<:Real}
+    rlon::Vector{<:Real}, rlat::Vector{<:Real};
+    throw::Bool=true
 )
 
     rW = mod(minimum(rlon),360); rS = minimum(rlat);
@@ -19,29 +34,82 @@ function ispointinregion(
     tlon = abs((rlon[2]-rlon[1])*3/4);
     tlat = abs((rlat[2]-rlat[1])*3/4);
 
-    if (rW < rE && (plon < (rW-tlon) || plon > (rE+tlon))) ||
-        (rW > rE && (plon < (rE-tlon) && plon > (rW+tlon))) ||
-        plat < (rS-tlat) || plat > (rN+tlat)
-
-          error("Requested coordinates [$(plon),$(plat)] are not within the specified longitude and/or latitude.")
-
-    end
+    return checkpoint(plon,plat,rN,rS,rE,rW,tlon,tlat,throw)
 
 end
 
-function ispointinregion(plon::Real,plat::Real,regionbounds::Vector{<:Real})
+"""
+    ispointinregion(plon, plat, regionbounds; tlon=0, tlat=0, throw=true) -> Bool
+
+Check if a point with coordinates (`plon`,`plat`) is found within a region defined by the bounds `regionbounds` in [`N`,`S`,`E`,`W`] format.
+
+Arguments:
+* `plon::Real` : Longitude of the point in question.
+* `plat::Real` : Latitude of the point in question.
+* `regionbounds::Vector{<:Real}` : Vector defining the [North,South,East,West] bounds of the region.
+
+Keyword Arguments:
+* `tlon::Real` : Threshold for longitude bounds in °.  If set to 0, means that there is no leniency for the point to fall outside the longitude bounds
+* `tlat::Real` : Threshold for latitude bounds in °.  If set to 0, means that there is no leniency for the point to fall outside the latitude bounds
+* `throw::Bool` : If `throw=true`, then if (`plon`,`plat`) is not within the region, an error is thrown and the program stops running.
+"""
+function ispointinregion(
+    plon::Real, plat::Real,
+    regionbounds::Vector{<:Real};
+    tlon::Real=0, tlat::Real=0,
+    throw::Bool=true
+)
 
     rN,rS,rE,rW = regionbounds; rW = mod(rW,360); rE = mod(rE,360);
 
-    if (rW < rE && (plon < rW || plon > rE)) || (rW > rE && (plon < rE && plon > rW)) ||
-        plat < S || plat > N
-
-          error("Requested coordinates [$(plon),$(plat)] are not within the specified region boundaries.")
-    end
+    return checkpoint(plon,plat,rN,rS,rE,rW,tlon,tlat,throw)
 
 end
 
-function ispointinregion(pcoord::Vector{<:Real},rlon::Vector{<:Real},rlat::Vector{<:Real})
+"""
+    ispointinregion(plon, plat, gregID; tlon=0, tlat=0, throw=true) -> Bool
+
+Check if a point with coordinates `pcoord = [plon,plat]` is found within a region defined by the bounds `regionbounds` in [`N`,`S`,`E`,`W`] format.
+
+Arguments:
+* `plon::Real` : Longitude of the point in question.
+* `plat::Real` : Latitude of the point in question.
+* `gregID::AbstractString` : `GeoRegion` `ID`
+
+Keyword Arguments:
+* `throw::Bool` : If `throw=true`, then if (`plon`,`plat`) is not within the region, an error is thrown and the program stops running.
+"""
+function ispointinregion(
+    plon::Real, plat::Real,
+    gregID::AbstractString;
+    tlon::Real=0, tlat::Real=0,
+    throw::Bool=true
+)
+
+    rN,rS,rE,rW = gregionbounds(gregID); rW = mod(rW,360); rE = mod(rE,360);
+
+    return checkpoint(plon,plat,rN,rS,rE,rW,tlon,tlat,throw)
+
+end
+
+"""
+    ispointinregion(pcoord, rlon, rlat; throw=true) -> Bool
+
+Check if a point with coordinates `pcoord = [plon,plat]` is found within a region defined by the longitude and latitude vectors `rlon` and `rlat`.
+
+Arguments:
+* `pcoord::Vector{<:Real}` : Vector defining coordinates of point in `[plon,plat]`
+* `rlon::Vector{<:Real}` : Longitude vector spanning the region.  Points should be evenly spaced.
+* `rlat::Vector{<:Real}` : Latitude vector spanning the region.  Points should be evenly spaced.
+
+Keyword Arguments:
+* `throw::Bool` : If `throw=true`, then if (`plon`,`plat`) is not within the region, an error is thrown and the program stops running.
+"""
+function ispointinregion(
+    pcoord::Vector{<:Real},
+    rlon::Vector{<:Real}, rlat::Vector{<:Real};
+    throw::Bool=true
+)
 
     plon,plat = pcoord;
     rW = mod(minimum(rlon),360); rS = minimum(rlat);
@@ -49,50 +117,109 @@ function ispointinregion(pcoord::Vector{<:Real},rlon::Vector{<:Real},rlat::Vecto
     tlon = abs((rlon[2]-rlon[1])*3/4);
     tlat = abs((rlat[2]-rlat[1])*3/4);
 
-    if (rW < rE && (plon < (rW-tlon) || plon > (rE+tlon))) ||
-        (rW > rE && (plon < (rE-tlon) && plon > (rW+tlon))) ||
-        plat < (rS-tlat) || plat > (rN+tlat)
-
-          error("Requested coordinates [$(plon),$(plat)] are not within the specified longitude and/or latitude.")
-
-    end
+    return checkpoint(plon,plat,rN,rS,rE,rW,tlon,tlat,throw)
 
 end
 
-function ispointinregion(pcoord::Vector{<:Real},regionbounds::Vector{<:Real})
+"""
+    ispointinregion(pcoord, regionbounds; tlon=0, tlat=0, throw=true) -> Bool
+
+Check if a point with coordinates `pcoord = [plon,plat]` is found within a region defined by the bounds `regionbounds` in [`N`,`S`,`E`,`W`] format.
+
+Arguments:
+* `pcoord::Vector{<:Real}` : Vector defining coordinates of point in `[plon,plat]`
+* `regionbounds::Vector{<:Real}` : Vector defining the [North,South,East,West] bounds of the region.
+
+Keyword Arguments:
+* `tlon::Real` : Threshold for longitude bounds in °.  If set to 0, means that there is no leniency for the point to fall outside the longitude bounds
+* `tlat::Real` : Threshold for latitude bounds in °.  If set to 0, means that there is no leniency for the point to fall outside the latitude bounds
+* `throw::Bool` : If `throw=true`, then if (`plon`,`plat`) is not within the region, an error is thrown and the program stops running.
+"""
+function ispointinregion(
+    pcoord::Vector{<:Real},
+    regionbounds::Vector{<:Real};
+    tlon::Real=0, tlat::Real=0,
+    throw::Bool=true
+)
 
     plon,plat = pcoord; rN,rS,rE,rW = regionbounds;
     rW = mod(rW,360); rE = mod(rE,360);
 
-    if (rW < rE && (plon < rW || plon > rE)) || (rW > rE && (plon < rE && plon > rW)) ||
-        plat < S || plat > N
+    return checkpoint(plon,plat,rN,rS,rE,rW,tlon,tlat,throw)
 
-          error("Requested coordinates [$(plon),$(plat)] are not within the specified region boundaries.")
+end
+
+"""
+    ispointinregion(pcoord, gregID; tlon=0, tlat=0, throw=true) -> Bool
+
+Check if a point with coordinates `pcoord = [plon,plat]` is found within a region defined by the bounds `regionbounds` in [`N`,`S`,`E`,`W`] format.
+
+Arguments:
+* `pcoord::Vector{<:Real}` : Vector defining coordinates of point in `[plon,plat]`
+* `gregID::AbstractString` : `GeoRegion` `ID`
+
+Keyword Arguments:
+* `tlon::Real` : Threshold for longitude bounds in °.  If set to 0, means that there is no leniency for the point to fall outside the longitude bounds
+* `tlat::Real` : Threshold for latitude bounds in °.  If set to 0, means that there is no leniency for the point to fall outside the latitude bounds
+* `throw::Bool` : If `throw=true`, then if (`plon`,`plat`) is not within the region, an error is thrown and the program stops running.
+"""
+function ispointinregion(
+    pcoord::Vector{<:Real},
+    gregID::AbstractString;
+    tlon::Real=0, tlat::Real=0,
+    throw::Bool=true
+)
+
+    plon,plat = pcoord; rN,rS,rE,rW = gregionbounds(gregID);
+    rW = mod(rW,360); rE = mod(rE,360);
+
+    return checkpoint(plon,plat,rN,rS,rE,rW,tlon,tlat,throw)
+
+end
+
+function checkpoint(
+    plon::Real, plat::Real,
+    rN::Real, rS::Real, rE::Real, rW::Real,
+    tlon::Real, tlat::Real,
+    throw::Bool
+)
+
+    if (rW < rE && (plon < (rW-tlon) || plon > (rE+tlon))) ||
+        (rW > rE && (plon < (rW-tlon) && plon > (rE+tlon))) ||
+        plat < (rS-tlat) || plat > (rN+tlat)
+
+        if throw
+             error("Requested coordinates [$(plon),$(plat)] are not within the specified region boundaries.")
+        else return false
+        end
+
+    else; return true
 
     end
 
 end
 
+
 ## Is Grid in GeoRegion?
 
-function isgridinregion(gridbounds::Vector{<:Real},regionbounds::Vector{<:Real})
+function isgridinregion(
+    gridbounds::Vector{<:Real},
+    regionbounds::Vector{<:Real};
+    tlon::Real=0, tlat::Real=0,
+    throw::Bool=true
+)
 
     N,S,E,W = gridbounds; rN,rS,rE,rW = regionbounds;
     E = mod(E,360); rE = mod(rE,360); W = mod(W,360); rW = mod(rW,360);
 
-    if (rW < rE && (E < rW || E > rE)) || (rW > rE && (E < rE && E > rW)) ||
-        (rW < rE && (W < rW || W > rE)) || (rW > rE && (W < rE && W > rW)) ||
-        N < rS || N > rN || S < rS || S > rN
-
-          error("Requested grid points [$(gridbounds)] are not within the specified region boundaries.")
-
-    end
+    return checkgrid(N,S,E,W,rN,rS,rE,rW,tlon,tlat,throw)
 
 end
 
 function isgridinregion(
     gridbounds::Vector{<:Real},
-    rlon::Vector{<:Real},rlat::Vector{<:Real}
+    rlon::Vector{<:Real},rlat::Vector{<:Real};
+    throw::Bool=true
 )
 
     N,S,E,W = gridbounds; E = mod(E,360); W = mod(W,360);
@@ -101,14 +228,44 @@ function isgridinregion(
     tlon = abs((rlon[2]-rlon[1])*3/4);
     tlat = abs((rlat[2]-rlat[1])*3/4);
 
-    if (rW < rE && (E < (rW-tlon) || E > (rE+tlon))) ||
-        (rW > rE && (E < (rE-tlon) && E > (rW+tlon))) ||
-        (rW < rE && (W < (rW-tlon) || W > (rE+tlon))) ||
-        (rW > rE && (W < (rE-tlon) && W > (rW+tlon))) ||
-        N < (rS-tlat) || N > (rN+tlat) ||
-        S < (rS-tlat) || S > (rN+tlat)
+    return checkgrid(N,S,E,W,rN,rS,rE,rW,tlon,tlat,throw)
 
-          error("Requested grid points [$(gridbounds)] are not within the specified longitude and/or latitude.")
+end
+
+function isgridinregion(
+    gridbounds::Vector{<:Real},
+    gregID::AbstractString;
+    tlon::Real=0, tlat::Real=0,
+    throw::Bool=true
+)
+
+    N,S,E,W = gridbounds; rN,rS,rE,rW = gregionbounds(gregID);
+    E = mod(E,360); rE = mod(rE,360); W = mod(W,360); rW = mod(rW,360);
+
+    return checkgrid(N,S,E,W,rN,rS,rE,rW,tlon,tlat,throw)
+
+end
+
+function checkgrid(
+    gN::Real, gS::Real, gE::Real, gW::Real,
+    rN::Real, rS::Real, rE::Real, rW::Real,
+    tlon::Real, tlat::Real,
+    throw::Bool
+)
+
+    if (rW < rE && (gE < (rW-tlon) || gE > (rE+tlon))) ||
+        (rW > rE && (gE < (rW-tlon) && gE > (rE+tlon))) ||
+        (rW < rE && (gW < (rW-tlon) || gW > (rE+tlon))) ||
+        (rW > rE && (gW < (rW-tlon) && gW > (rWE+tlon))) ||
+        gN < (rS-tlat) || gN > (rN+tlat) ||
+        gS < (rS-tlat) || gS > (rN+tlat)
+
+        if throw
+             error("Requested grid points [$(gridbounds)] are not within the specified region boundaries.")
+        else return false
+        end
+
+    else; return true
 
     end
 
