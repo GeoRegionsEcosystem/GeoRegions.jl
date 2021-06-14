3f8ce194846ad1d1932069f8fc457d3186832c6e
Nathanael Wong <natgeo.wong@outlook.com>
2021-06-14 02:41:41 -0400

    Updating extraction method for GeoRegion types

--- src/extract.jl
+++ src/extract.jl
@@ -1,23 +1,82 @@
-"""
-This set of functions extracts data for a given point / within a defined grid from a region
-defined either by a set of region [N,S,E,W] bounds or by longitude/latitude vectors.
+function GeoRegionInfo(
+    GeoReg :: RectRegion{ST,FT},
+    lon    :: Vector{<:Real},
+    lat    :: Vector{<:Real}
+) where {FT <: Real, ST <: AbstractString}
 
-Other functionalities include:
-    * Returning information of grid longitude/latitude vectors (for grid extraction)
+    @debug "$(Dates.now()) - Determining indices of longitude and latitude boundaries in the given dataset ..."
+    N = GeoReg.N
+    S = GeoReg.S
+    E = GeoReg.E
+    W = GeoReg.W
 
-"""
+    igrid = regiongrid([N,S,E,W],lon,lat);
+    iN = igrid[1]; iS = igrid[2]; iE = igrid[3]; iW = igrid[4]
 
-## Find Index of given position in Region
+    @debug "$(Dates.now()) - Creating vector of latitude indices to extract ..."
+    if     iN < iS; iNS = iN : iS
+    elseif iS < iN; iNS = iS : iN
+    else;           iNS = iN;
+    end
 
-function regionpoint(
-    plon::Real, plat::Real,
-    rlon::Vector{<:Real}, rlat::Vector{<:Real}
-)
+    @debug "$(Dates.now()) - Creating vector of longitude indices to extract ..."
+    if     iW < iE; iWE = vcat(iW:iE)
+    elseif iW > iE || (iW == iE && bounds[3] != bounds[4])
+        iWE = vcat(iW:length(lon),1:iE)
+        lon[1:(iW-1)] .+= 360
+    else
+        iWE = [iW];
+    end
 
-    plon = mod(plon,360); ispointinregion(plon,plat,rlon,rlat); rlon = mod.(rlon,360);
-    ilon = argmin(abs.(rlon.-plon)); ilat = argmin(abs.(rlat.-plat));
+    while lon[end] > 360; lon .-= 360 end
+    nlon = lon[iWE]
+    nlat = lat[iNS]
 
-    return [ilon,ilat]
+    return RectInfo{FT}(igrid,iWE,iNS,nlon,nlat)
+
+end
+
+function GeoRegionInfo(
+    GeoReg :: PolyRegion{ST,FT},
+    lon    :: Vector{<:Real},
+    lat    :: Vector{<:Real}
+) where {FT <: Real, ST <: AbstractString}
+
+    @debug "$(Dates.now()) - Determining indices of longitude and latitude boundaries in the given dataset ..."
+    N = GeoReg.N
+    S = GeoReg.S
+    E = GeoReg.E
+    W = GeoReg.W
+
+    igrid = regiongrid([N,S,E,W],lon,lat);
+    iN = igrid[1]; iS = igrid[2]; iE = igrid[3]; iW = igrid[4]
+
+    @debug "$(Dates.now()) - Creating vector of latitude indices to extract ..."
+    if     iN < iS; iNS = vcat(iN:iS)
+    elseif iS < iN; iNS = vcat(iS:iN)
+    else;           iNS = [iN];
+    end
+
+    @debug "$(Dates.now()) - Creating vector of longitude indices to extract ..."
+    if     iW < iE; iWE = vcat(iW:iE)
+    elseif iW > iE || (iW == iE && bounds[3] != bounds[4])
+        iWE = vcat(iW:length(lon),1:iE)
+        lon[1:(iW-1)] .+= 360
+    else
+        iWE = [iW];
+    end
+
+    while lon[end] > 360; lon .-= 360 end
+
+    nlon = lon[iWE]
+    nlat = lat[iNS]
+    mask = zeros(Int,length(nlon),length(nlat))
+    for ilat = 1 : length(nlat), ilon = 1 : length(nlon)
+        ipnt = Point2(nlon[ilon],nlat[ilat])
+        mask[ilon,ilat] = isPointinGeoRegion(ipnt,GeoReg,throw=false) * 1
+    end
+
+    return PolyInfo{FT}(igrid,iWE,iNS,nlon,nlat,mask)
 
 end
 
@@ -36,157 +95,5 @@ function regiongrid(gridbounds::Vector{<:Real},rlon::Vector{<:Real},rlat::Vector
     end
 
     return [iN,iS,iE,iW]
-
-end
-
-function regiongridvec(gridbounds::Vector{<:Real},rlon::Vector{<:Real},rlat::Vector{<:Real})
-
-    iN,iS,iE,iW = regiongrid(gridbounds,rlon,rlat);
-    nlon = deepcopy(rlon)
-
-    if     iN < iS; iNS = iN : iS
-    elseif iS < iN; iNS = iS : iN
-    else;           iNS = iN;
-    end
-
-    if     iW < iE; iWE = iW : iE
-    elseif iW > iE || (iW == iE && gridbounds[3] != gridbounds[4])
-        iWE = 1 : (iE + length(nlon) + 1 - iW);
-        nlon[1:(iW-1)] = nlon[1:(iW-1)] .+ 360; nlon = circshift(nlon,1-iW);
-    else
-        iWE = iW;
-    end
-
-    reginfo = Dict("gridindices"=>[iN,iS,iE,iW],"lonlatindices"=>[iWE,iNS]);
-
-    glon = nlon[iWE]; #while glon[end] > 360; glon = glon .- 360; end
-
-    return glon,rlat[iNS],reginfo
-
-end
-
-## Extraction of Data
-
-function regionextract(data::Array{<:Real},coord::Array,ndim::Integer)
-
-    nlon1 = length(coord[1]); nlon2 = size(data,1);
-    nlat1 = length(coord[2]); nlat2 = size(data,2);
-    nt = size(data)[3:end];
-
-    if ndim == 2; data = data[coord[1],coord[2]];
-    elseif ndim >= 3;
-        data = reshape(data,nlon2,nlat2,:)
-        data = data[coord[1],coord[2],:];
-        data = reshape(data,Tuple(vcat(nlon1,nlat1,nt...)));
-    end
-
-    return data
-
-end
-
-function regionextract(data::Array{<:Real},coord::Array)
-
-    nlon1 = length(coord[1]); nlon2 = size(data,1);
-    nlat1 = length(coord[2]); nlat2 = size(data,2);
-    nt = size(data)[3:end]; ndim = ndims(data);
-
-    if ndim == 2; data = data[coord[1],coord[2]];
-    elseif ndim >= 3;
-        data = reshape(data,nlon2,nlat2,:);
-        data = data[coord[1],coord[2],:];
-        data = reshape(data,Tuple(vcat(nlon1,nlat1,nt...)));
-    end
-
-    return data
-
-end
-
-function regionextractpoint(
-    rdata::Array{<:Real},
-    plon::Real, plat::Real,
-    rlon::Vector{<:Real}, rlat::Vector{<:Real}
-)
-
-    icoord = regionpoint(plon,plat,rlon,rlat); ndim = ndims(data)
-    pdata = regionextract(rdata,icoord,ndim)
-
-    return pdata
-
-end
-
-function regionextractgrid(
-    rdata::Array{<:Real}, reginfo::Dict
-)
-
-    iW = reginfo["gridindices"][4]; iE = reginfo["gridindices"][3];
-
-    if iW > iE; rdata = circshift(rdata,1-iW);
-          gdata = regionextract(rdata,reginfo["lonlatindices"]);
-    else; gdata = regionextract(rdata,reginfo["lonlatindices"]);
-    end
-
-    return gdata
-
-end
-
-function regionextractgrid(
-    rdata::Array{<:Real},
-    gridbounds::Vector{<:Real}, rlon::Vector{<:Real}, rlat::Vector{<:Real}
-)
-
-    nlon = length(rlon); ndim = ndims(rdata); igrid  = regiongrid(gridbounds,rlon,rlat);
-    iN = igrid[1]; iS = igrid[2]; iE = igrid[3]; iW = igrid[4];
-
-    if     iN < iS; iNS = iN : iS
-    elseif iS < iN; iNS = iS : iN
-    end
-
-    if iW > iE; iWE = 1 : (iE + nlon + 1 - iW); rdata = circshift(rdata,1-iW);
-          gdata = regionextract(rdata,[iWE,iNS],ndim)
-    else; iWE = iW : iE;
-          gdata = regionextract(rdata,[iWE,iNS],ndim)
-
-    end
-
-    return gdata
-
-end
-
-function regionextractgrid!(
-    rdata::Array{<:Real},
-    gridbounds::Vector{<:Real}, rlon::Vector{<:Real}, rlat::Vector{<:Real},
-    tmp::Array{<:Real}
-)
-
-    nlon = length(rlon); ndim = ndims(rdata); igrid  = regiongrid(gridbounds,rlon,rlat);
-    iN = igrid[1]; iS = igrid[2]; iE = igrid[3]; iW = igrid[4];
-
-    if     iN < iS; iNS = iN : iS
-    elseif iS < iN; iNS = iS : iN
-    end
-
-    if iW > iE; iWE = 1 : (iE + nlon + 1 - iW); circshift!(tmp,data,1-iW);
-          gdata = regionextract(tmp,[iWE,iNS],ndim)
-    else; iWE = iW : iE;
-          gdata = regionextract(rdata,[iWE,iNS],ndim)
-
-    end
-
-    return gdata
-
-end
-
-function regionextractgrid!(
-    rdata::Array{<:Real}, reginfo::Dict, tmp::Array{<:Real}
-)
-
-    iW = reginfo["gridindices"][4]; iE = reginfo["gridindices"][3];
-
-    if iW > iE; circshift!(tmp,data,1-iW);
-          gdata = regionextract(tmp,reginfo["lonlatindices"]);
-    else; gdata = regionextract(rdata,reginfo["lonlatindices"]);
-    end
-
-    return gdata
 
 end
