f8e7693ca901c673cbe2dd8bd0a035d3cc007ef4
Nathanael Wong <natgeo.wong@outlook.com>
2021-06-14 02:44:45 -0400

    Stage for new file capitalizations

---
+++ src/Query.jl
@@ -0,0 +1,23 @@
+function coordGeoRegion(GeoReg::PolyRegion)
+    shape = GeoReg.shape
+    npnt  = length(shape)
+    lon   = zeros(npnt)
+    lat   = zeros(npnt)
+
+    for ipnt = 1 : npnt
+        lon[ipnt] = shape[ipnt][1]
+        lat[ipnt] = shape[ipnt][2]
+    end
+
+    return lon,lat
+end
+
+function coordGeoRegion(GeoReg::RectRegion)
+
+    N = GeoReg.N
+    S = GeoReg.S
+    E = GeoReg.E
+    W = GeoReg.W
+    return [W,E,E,W,W],[N,N,S,S,N]
+
+end
