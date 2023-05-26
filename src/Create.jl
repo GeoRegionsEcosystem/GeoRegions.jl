40694e74fb71369ab3967586353f59f8d03c6f42
Nathanael Wong <natgeo.wong@outlook.com>
2023-05-26 00:32:48 -0400

    If parent is GLB, no need to check for validity

--- src/Create.jl
+++ src/Create.jl
@@ -3,8 +3,9 @@
         RegID :: AbstractString,
         ParID :: AbstractString,
         name  :: AbstractString,
-        bound :: Vector{<:Real},
+        bound :: Vector{<:Real};
         savegeo :: Bool = true,
+        verbose :: Bool = true,
         ST = String,
         FT = Float64
     ) -> RectRegion{ST,FT}
@@ -13,14 +14,16 @@ Creates a rectilinear GeoRegion `RegID`.
 
 Arguments
 =========
-
 - `RegID` : The keyword ID that will be used to identify the GeoRegion.
             If the ID is already in use, then an error will be thrown.
-
 - `ParID` : The ID of the parent GeoRegion where information can be extracted from
 - `name`  : A name for the GeoRegion (meta information, can be used in Logging)
 - `bound` : The [N,S,E,W] coordinates defining the region
-- `savegeo` : Save the GeoRegion into the master list?
+
+Keyword Arguments
+=========
+- `savegeo` : Save the GeoRegion into the master list? Default is `true`
+- `verbose` : Verbose logging for ease of monitoring? Default is `true`
 """
 function RectRegion(
     RegID :: AbstractString,
@@ -28,9 +31,14 @@ function RectRegion(
     name  :: AbstractString,
     bound :: Vector{<:Real};
     savegeo :: Bool = true,
+    verbose :: Bool = true,
     ST = String,
     FT = Float64
 )
+
+    if !verbose
+        disable_logging(Logging.Warn)
+    end
 
     if isGeoRegion(RegID,throw=false)
         error("$(modulelog()) - The GeoRegion $(RegID) has already been defined.  Please use another identifier.")
@@ -38,8 +46,10 @@ function RectRegion(
         @info "$(modulelog()) - Adding the GeoRegion $(RegID) to the list."
     end
 
-    if !isGeoRegion(ParID,throw=false)
-        error("$(modulelog()) - The GeoRegion $(ParID) was defined to be the parent GeoRegion of $(RegID), but the GeoRegion $(ParID) is not defined.  Please define the GeoRegion $(ParID) and its properties.")
+    if ParID != "GLB"
+        if !isGeoRegion(ParID,throw=false)
+            error("$(modulelog()) - The GeoRegion $(ParID) was defined to be the parent GeoRegion of $(RegID), but the GeoRegion $(ParID) is not defined.  Please define the GeoRegion $(ParID) and its properties.")
+        end
     end
 
     N,S,E,W = bound; is180,is360 = checkbounds(N,S,E,W)
@@ -51,6 +61,10 @@ function RectRegion(
         open(joinpath(DEPOT_PATH[1],"files","GeoRegions","rectlist.txt"),"a") do io
             write(io,"$RegID, $ParID, $N, $W, $S, $E, $name\n")
         end
+    end
+
+    if !verbose
+        disable_logging(Logging.Debug)
     end
 
     return geo
@@ -65,6 +79,7 @@ end
         lonpt :: Vector{<:Real},
         latpt :: Vector{<:Real};
         savegeo :: Bool = true,
+        verbose :: Bool = true,
         ST = String,
         FT = Float64
     ) -> PolyRegion{ST,FT}
@@ -81,7 +96,11 @@ Arguments
 - `name`  : A name for the GeoRegion (meta information, can be used in Logging)
 - `lonpt` : A vector containing the longitude points
 - `latpt` : A vector containing the latitude points
-- `savegeo` : Save the GeoRegion into the master list?
+
+Keyword Arguments
+=========
+- `savegeo` : Save the GeoRegion into the master list? Default is `true`
+- `verbose` : Verbose logging for ease of monitoring? Default is `true`
 
 !!! info "Start and End Points"
     The 1st and last elements of `lonpt` and `latpt` must be equal.
@@ -93,18 +112,25 @@ function PolyRegion(
     lonpt :: Vector{<:Real},
     latpt :: Vector{<:Real},;
     savegeo :: Bool = true,
+    verbose :: Bool = true,
     ST = String,
     FT = Float64
 )
 
+    if !verbose
+        disable_logging(Logging.Warn)
+    end
+    
     if isGeoRegion(RegID,throw=false)
         error("$(modulelog()) - The GeoRegion $(RegID) has already been defined.  Please use another identifier.")
     else
         @info "$(modulelog()) - Adding the GeoRegion $(RegID) to the list."
     end
 
-    if !isGeoRegion(ParID)
-        error("$(modulelog()) - The GeoRegion $(ParID) was defined to be the parent GeoRegion of $(RegID), but the GeoRegion $(ParID) is not defined.  Please define the GeoRegion $(ParID) and its properties.")
+    if ParID != "GLB"
+        if !isGeoRegion(ParID)
+            error("$(modulelog()) - The GeoRegion $(ParID) was defined to be the parent GeoRegion of $(RegID), but the GeoRegion $(ParID) is not defined.  Please define the GeoRegion $(ParID) and its properties.")
+        end
     end
 
     if lonpt[1] != lonpt[end]
@@ -134,6 +160,10 @@ function PolyRegion(
             for ipt = 2 : npt; write(io,", $(latpt[ipt])") end
             write(io,"\n")
         end
+    end
+
+    if !verbose
+        disable_logging(Logging.Debug)
     end
 
     return geo
