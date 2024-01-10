51fe4706802483e520dbcb5947a09a8dc90a4df1
Nathanael Wong <natgeo.wong@outlook.com>
2024-01-10 10:19:06 -0100

    Adding some more basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,30 @@
 using GeoRegions
 using Test
 
-@testset "GeoRegions.jl" begin
+@testset "Test that all predefined GeoRegions work" begin
+
+    # Test all predefined GeoRegions
+    geov,_,_ = listGeoRegions()
+    for geo in geov
+        @test isGeoRegion(geo);
+    end
+
+end
+
+@testset "Test Creation, Detection and Removal of GeoRegions" begin
+
+    @test !isGeoRegion("TRP",throw=false)
+    geo = RectRegion("TRP","GLB","Tropics",[30,-30,360,0])
+    geo = RectRegion("TRP_DTP","GLB","Tropics",[10,-10,360,0])
+    @test isGeoRegion("TRP",throw=false)
+    removeGeoRegion("TRP")
+    @test isGeoRegion("TRP_DTP",throw=false)
+    removeGeoRegion("TRP_DTP")
+    @test !isGeoRegion("TRP_DTP",throw=false)
+
+end
+
+@testset "Testing isPointinGeoRegion" begin
     # Test in operator
     A = Point2(-20,5)
     B = Point2(340,5)
