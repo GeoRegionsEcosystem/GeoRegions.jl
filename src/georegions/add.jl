a4e5b9efe111f03caa5500278b7e14cfdcd56d38
Nathanael Wong <natgeo.wong@outlook.com>
2024-09-04 01:36:10 -0400

    Add some general-name functions, also allow for adding of GeoRegions that have already been defined in script but not saved

---
+++ src/georegions/add.jl
@@ -0,0 +1,55 @@
+add(
+    geo  :: GeoRegion;
+    path :: AbstractString,
+    verbose :: Bool = true
+) = addGeoRegion(geo;path=path,verbose=verbose)
+
+function addGeoRegion(
+    geo  :: RectRegion;
+    path :: AbstractString,
+    verbose :: Bool = true
+)
+
+    RectRegion(
+        geo.ID, geo.pID, geo.name,
+        [geo.N,geo.S,geo.E,geo.W],
+        path = path, verbose = verbose
+    )
+
+    return nothing
+
+end
+
+function addGeoRegion(
+    geo  :: TiltRegion;
+    path :: AbstractString,
+    verbose :: Bool = true
+)
+
+    TiltRegion(
+        geo.ID, geo.pID, geo.name,
+        geo.X, geo.Y, geo.ΔX, geo.ΔY, geo.θ,
+        path = path, verbose = verbose
+    )
+
+    return nothing
+
+end
+
+function addGeoRegion(
+    geo  :: PolyRegion;
+    path :: AbstractString,
+    verbose :: Bool = true
+)
+
+    lon,lat = coordGeoRegion(geo,n=1)
+
+    PolyRegion(
+        geo.ID, geo.pID, geo.name,
+        lon, lat,
+        path = path, verbose = verbose
+    )
+
+    return nothing
+
+end
--- src/georegions/remove.jl
+++ src/georegions/remove.jl
@@ -1,4 +1,28 @@
 """
+    rm(
+        geo  :: GeoRegion;
+        path :: AbstractString = geodir
+    )
+
+Removes the GeoRegion `geo` from the custom lists specified in `path`. The GeoRegion must have exactly the same properties as the one in the custom list
+
+Arguments
+=========
+- `geo` : The GeoRegion to be removed from the custom lists in `path`
+
+Keyword Arguments
+=================
+- `path` : The path where the list of custom GeoRegions will be retrieved from.
+           Defaults to the `local` package variable `geodir`
+"""
+rm(
+    geo  :: GeoRegion,
+    path :: AbstractString = geodir
+) = if isGeoRegion(geo,path=path)
+    removeGeoRegion(geo.ID,path=path)
+end
+
+"""
     removeGeoRegion(
         geoID :: AbstractString;
         path  :: AbstractString = geodir
