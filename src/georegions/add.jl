24c89eff1f1792319301b2da31b918a8fbebea27
Nathanael Wong <natgeo.wong@outlook.com>
2025-02-12 16:27:28 -0500

    Reducing methods to one since we combined all GeoRegion Types

--- src/georegions/add.jl
+++ src/georegions/add.jl
@@ -18,49 +18,16 @@ Keyword Arguments
 - `verbose` : Verbose logging for ease of monitoring? Default is `false`.
 """
 function add(
-    geo :: RectRegion;
-    path    :: AbstractString = dirname(geo.path),
-    verbose :: Bool = false
-)
-
-    RectRegion(
-        geo.ID, geo.pID, geo.name,
-        geo.bound,
-        path = path, verbose = verbose, save = true
-    )
-
-    return nothing
-
-end
-
-function add(
-    geo :: TiltRegion;
-    path    :: AbstractString = dirname(geo.path),
-    verbose :: Bool = false
-)
-
-    TiltRegion(
-        geo.ID, geo.pID, geo.name,
-        geo.X, geo.Y, geo.ΔX, geo.ΔY, geo.θ,
-        path = path, verbose = verbose, save = true
-    )
-
-    return nothing
-
-end
-
-function add(
-    geo :: PolyRegion;
-    path    :: AbstractString = dirname(geo.path),
+    geo  :: GeoRegion;
+    path :: AbstractString = dirname(geo.path),
     verbose :: Bool = false
 )
 
     lon,lat = coordinates(geo)
 
-    PolyRegion(
-        geo.ID, geo.pID, geo.name,
-        lon, lat,
-        path = path, verbose = verbose, save = true
+    GeoRegion(
+        lon, lat, ID = geo.ID, pID = geo.pID, name = geo.name, rotation = geo.θ,
+        path = geopath(path), verbose = verbose, save = true
     )
 
     return nothing
@@ -92,8 +59,9 @@ function overwrite(
     verbose :: Bool = false
 )
 
-    rmID(geo.ID,path=path)
-    add(geo,path=path,verbose=verbose)
+    gpath = geopath(path)
+    rmID(geo.ID,path=gpath)
+    add(geo,path=gpath,verbose=verbose)
     
     return nothing
 
