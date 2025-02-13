b28e86207361549253e6570791f5f33f8e97af31
Nathanael Wong <natgeo.wong@outlook.com>
2025-02-12 22:23:10 -0500

    Since for now we only have the GeoRegion Type and not the MultiRegion type we keep it to 6 columns

--- src/georegions/tables.jl
+++ src/georegions/tables.jl
@@ -2,9 +2,13 @@
     tableGeoRegions(;
         path :: AbstractString = homedir(),
         predefined :: Bool = true,
+        giorgi     :: Bool = true,
+        srex       :: Bool = true,
+        ar6        :: Bool = true,
         custom     :: Bool = true,
         warn :: Bool = true,
-        crop :: Bool = false
+        crop :: Bool = false,
+        rows :: Int = 0
     ) -> nothing
 
 Display all available GeoRegions in tabular format.
@@ -14,6 +18,9 @@ Keyword Arguments
 - `path` : The path where the list of custom GeoRegions will be retrieved from.
            Defaults to the user's home directory `homedir()`.
 - `predefined` : If `true`, predefined Giorgi, SREX and IPPC AR6 list of GeoRegions will be displayed.
+- `giorgi` : If `true` AND `predefined = true`, display predefined GF GeoRegions. Default is `true`.
+- `srex` : If `true` AND `predefined = true`, display predefined SREX GeoRegions. Default is `true`.
+- `ar6` : If `true` AND `predefined = true`, display predefined IPCC AR6 GeoRegions. Default is `true`.
 - `custom` : If `true`, custom, user-defined list of GeoRegions will be displayed.
 - `warn` : If `true`, display warnings if custom files do not exist.
 - `crop` : If `true`, will crop the vertical extent of the table, default is `false`.
@@ -26,7 +33,8 @@ function tableGeoRegions(;
     ar6        :: Bool = true,
     custom     :: Bool = true,
     warn :: Bool = true,
-    crop :: Bool = false
+    crop :: Bool = false,
+    rows :: Int = 0
 )
 
     IDs    = []
@@ -41,75 +49,38 @@ function tableGeoRegions(;
     end
 
     ngeo = length(IDs)
-    fmat = Array{Any,2}(undef,ngeo,7)
+    fmat = Array{Any,2}(undef,ngeo,6)
 
     for igeo = 1 : ngeo
         geo = GeoRegion(IDs[igeo],path=gpaths[igeo])
         fmat[igeo,1] = geo.ID
-        fmat[igeo,2] = typeof(geo)
-        fmat[igeo,3] = geo.name
-        fmat[igeo,4] = geo.pID
-        fmat[igeo,5] = [geo.N, geo.S, geo.E, geo.W]
-        fmat[igeo,6] = geo.θ
-        fmat[igeo,7] = splitdir(gpaths[igeo])[end]
+        # fmat[igeo,2] = typeof(geo)
+        fmat[igeo,2] = geo.name
+        fmat[igeo,3] = geo.pID
+        fmat[igeo,4] = [geo.N, geo.S, geo.E, geo.W]
+        fmat[igeo,5] = geo.θ
+        fmat[igeo,6] = splitdir(gpaths[igeo])[end]
     end
 
-    head = ["ID","Type","Name","Parent","Bounds [N,S,E,W]","Rotation θ","Folder"];
+    head = ["ID","Name","Parent","Bounds [N,S,E,W]","Rotation θ","Folder"]
+    # head = ["ID","Type","Name","Parent","Bounds [N,S,E,W]","Rotation θ","Folder"]
 
-    if !crop
+    if !crop || rows < 15
         pretty_table(
             fmat,header=head,
-            alignment=[:c,:c,:l,:c,:c,:c,:c],
+            alignment=[:c,:l,:c,:c,:c,:c],
+            # alignment=[:c,:c,:l,:c,:c,:c,:c],
             crop = :none, tf = tf_compact
         );
     else
         pretty_table(
             fmat,header=head,
-            alignment=[:c,:c,:l,:c,:c,:c,:c],
-            crop = :vertical, tf = tf_compact
+            alignment=[:c,:l,:c,:c,:c,:c],
+            # alignment=[:c,:c,:l,:c,:c,:c,:c],
+            crop = :vertical, tf = tf_compact,
+            vcrop_mode = :middle, display_size = (rows,-1)
         );
     end
-
-    return nothing
-
-end
-
-"""
-    tableGeoRegions(
-        fname :: AbstractString
-    ) -> nothing
-
-Display all available GeoRegions in tabular format listed in the file `fname`.
-
-Arguments
-=========
-- `fname` : Specifies the name + path of the file containing GeoRegion information.
-"""
-function tableGeoRegions(
-    fname :: AbstractString
-)
-
-    rvec,rtype = listgeoregions(fname)
-    ngeo = size(rvec,1)
-    fmat = Array{Any,2}(undef,ngeo,6)
-
-    for igeo = 1 : ngeo
-        g = getgeoregion(rvec[igeo],fname,rtype)
-        fmat[igeo,1] = g.ID
-        fmat[igeo,2] = rtype
-        fmat[igeo,3] = g.name
-        fmat[igeo,4] = g.pID
-        fmat[igeo,5] = geo.bound
-        fmat[igeo,6] = basename(fname)
-    end
-
-    head = ["ID","Type","Name","Parent","Bounds [N,S,E,W]","File"];
-
-    pretty_table(
-        fmat,header=head,
-        alignment=[:c,:c,:l,:c,:c,:c],
-        crop = :none, tf = tf_compact
-    );
 
     return nothing
 
