38b05fb9e4b5c4f18dfa004a4efb352319d7e27b
Nathanael Wong <natgeo.wong@outlook.com>
2025-02-14 17:26:22 -0500

    Updates to show() to give more information

--- src/georegions/show.jl
+++ src/georegions/show.jl
@@ -1,12 +1,14 @@
 function show(io::IO, geo::GeoRegion)
+	shape = geo.geometry.shape
     print(io,
 		"The GeoRegion $(geo.ID) has the following properties:\n",
-		"    Region ID      (ID) : ", geo.ID, 		  	 		  '\n',
-		"    Parent ID     (pID) : ", geo.pID, 			 		  '\n',
-		"    Name         (name) : ", geo.name, 				  '\n',
-		"    Bounds    (N,S,E,W) : ", geo.N, ", ", geo.S, ", ", geo.E, ", ", geo.W, '\n',
-		"    Rotation        (θ) : ", geo.θ, 		 			  '\n',
-		"    Shape       (shape) : ", geo.geometry.shape, 		  '\n',
-		"    File Path    (path) : ", geo.path, 		 		  '\n',
+		"    Region ID         (ID) : ", geo.ID, 	'\n',
+		"    Parent ID        (pID) : ", geo.pID, 	'\n',
+		"    Name            (name) : ", geo.name, '\n',
+		"    Bounds       (N,S,E,W) : ", geo.N, ", ", geo.S, ", ", geo.E, ", ", geo.W, '\n',
+		"    Rotation           (θ) : ", geo.θ, 	'\n',
+		"    Level (geometry.level) : ", geo.geometry.level, '\n',
+		"    Shape (geometry.shape) : ", typeof(shape), "($(length(shape)))", '\n',
+		"    File Path       (path) : ", geo.path, '\n',
 	)
 end
