316872826a4382569477eb6bc0a40ed583648a92
Nathanael Wong <natgeo.wong@outlook.com>
2025-07-12 23:08:19 -0400

    Updating for the newest version of DocumenterVitepress

--- docs/make.jl
+++ docs/make.jl
@@ -68,7 +68,7 @@ for file in files
     rm(file)
 end
 
-deploydocs(;
+DocumenterVitepress.deploydocs(;
     repo      = "github.com/GeoRegionsEcosystem/GeoRegions.jl.git",
     target    = "build", # this is where Vitepress stores its output
     devbranch = "main",
